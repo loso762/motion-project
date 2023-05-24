4e4331ebdb969a8d7d2bb60775cb6b02a43812d4
loso762 <loso762@naver.com>
2023-05-24 16:00:19 +0900

    js 변환

--- dist/components/page/item/video.js
+++ dist/components/page/item/video.js
@@ -1,13 +1,13 @@
-import { BaseComponent } from './../../component.js';
+import { BaseComponent } from "./../../component.js";
 export class VideoComponent extends BaseComponent {
     constructor(title, url) {
         super(`<section class="video">
             <div class="video__player"><iframe class="video__iframe"></iframe></div>
             <h3 class="video__title"></h3>
         </section>`);
-        const iframe = this.element.querySelector('.video__iframe');
+        const iframe = this.element.querySelector(".video__iframe");
         iframe.src = this.convertToEmbeddedURL(url);
-        const titleElement = this.element.querySelector('.video__title');
+        const titleElement = this.element.querySelector(".video__title");
         titleElement.textContent = title;
     }
     convertToEmbeddedURL(url) {
