32d903da4bbdc50d4457b7e0d4969fd8f5a842e4
loso762 <loso762@naver.com>
2023-05-24 15:54:17 +0900

    SectionInput 중복 제거 리팩토링

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -1,4 +1,4 @@
-import { BaseComponent } from './../../component.js';
+import {BaseComponent} from "./../../component.js";
 export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
@@ -6,22 +6,16 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
             <h3 class="video__title"></h3>
         </section>`);
 
-    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    const iframe = this.element.querySelector(".video__iframe")! as HTMLIFrameElement;
     iframe.src = this.convertToEmbeddedURL(url);
 
-    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    const titleElement = this.element.querySelector(".video__title")! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
 
-  // input
-  // https://www.youtube.com/watch?v=K3-jG52XwuQ
-  // https://youtu.be/K3-jG52XwuQ
-  // output
-  // https://www.youtube.com/embed/K3-jG52XwuQ
-  // 정규표현식 Regex
-  // https://regexr.com/5l6nr
   private convertToEmbeddedURL(url: string): string {
-    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
     const match = url.match(regExp);
     const videoId = match ? match[1] || match[2] : undefined;
     if (videoId) {
